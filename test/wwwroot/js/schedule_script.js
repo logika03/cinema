fd4cdd0115d0fcd6fc3d94c6f8e7e2eb7f410bdd
logika03 <65672195+logika03@users.noreply.github.com>
2020-12-20 15:49:27 +0300

    delete

--- test/wwwroot/js/schedule_script.js
+++
@@ -1,6 +0,0 @@
-$(function (){
-    $('#schedule-page-button').dropdown('toggle');
-    $('#schedule-page-button').dropdown('toggle');
-    $('#schedule-page-dropdown-menu').appendTo($('body'));
-});
-
